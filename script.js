50df3423d345529ca5a164be1bc80ee9b696578d
johnlee0115 <johnlee011500@gmail.com>
2023-05-16 18:50:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var niel = document.querySelector("#niellikes");
+
+function nieladdLike(element){
+    niel.innerText++;
+}
+
+var nichole = document.querySelector("#nicholelikes");
+
+function nicholeaddLike(element){
+    nichole.innerText++;
+}
+
+var jim = document.querySelector("#jimlikes");
+
+function jimaddLike(element){
+    jim.innerText++;
+}
